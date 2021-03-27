3b3d816da22b23664c2297e3737ab0d523c53c5b
ahmedswaby <ahmed.khaledswaby@gmail.com>
2021-03-27 20:32:47 +0200

    add pagination

--- src/views/index.js
+++ src/views/index.js
@@ -14,25 +14,37 @@ const formatDate = (date) => {
 	return [year, month, day].join('-');
 };
 function Index() {
+	const [loading, setloading] = useState(true)
 	const [popularRepos, setPopularRepos] = useState([]);
+	const [pageNum, setpNum] = useState(+1);
+	// fetch data
+	async function fetchApi() {
+		setloading(true);
+		const date = new Date();
+		date.setDate(date.getDate() - 1);
+		const todayDate = formatDate(date.setDate(date.getDate()));
+
+		await fetch(
+			`https://api.github.com/search/repositories?q=created:%3E${todayDate}&sort=stars&order=desc&page=${pageNum}`
+		)
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				const moreRepos = data.items;
+				// setPopularRepos(moreRepos);
+				setPopularRepos([...popularRepos, ...moreRepos]);
+				setloading(false);
+			});
+	}
 
 	useEffect(() => {
-		// @ to-do  get days by today
-		async function fetchApi() {
-			const date = new Date();
-			date.setDate(date.getDate() - 1);
-			const todayDate = formatDate(date.setDate(date.getDate()));
-			console.log(todayDate);
-			const data = await fetch(
-				`https://api.github.com/search/repositories?q=created:%3E${todayDate}&sort=stars&order=desc`
-			)
-				.then((res) => res.json())
-				.then((data) => data);
+		fetchApi()
+	}, [pageNum]);
 
-			setPopularRepos(data.items);
-		}
-		fetchApi();
-	}, []);
+	const addMoreRepos = () => {
+		setpNum(pageNum + 1);
+	};
 
 	return (
 		<div className='container mt-5'>
@@ -40,6 +52,20 @@ function Index() {
 			{popularRepos.map((repo) => (
 				<Items key={repo.id} repo={repo}></Items>
 			))}
+			<div className='d-flex justify-content-center flex-column m-auto' style={{
+				maxHeight : 'fit-content',
+				alignItems: 'center'
+			}}
+			>
+				{loading ? 
+					<div class='spinner-border text-primary mb-3' role='status'>
+						<span class='visually-hidden'></span>
+					</div>
+				: false }
+				<button className='btn btn-dark' onClick={() => addMoreRepos()}>
+					More Repos
+				</button>
+			</div>
 		</div>
 	);
 }
